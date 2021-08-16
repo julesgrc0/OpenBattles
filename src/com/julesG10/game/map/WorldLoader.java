16439a1eae1d20a03128ea3564d0aa3ba749728a
JulesG10 <jules10avrg@gmail.com>
2021-08-16 22:58:21 +0200

    loader update

--- src/com/julesG10/game/map/WorldLoader.java
+++ src/com/julesG10/game/map/WorldLoader.java
@@ -1,2 +1,24 @@
-package com.julesG10.game.map;public class WorldLoader {
+package com.julesG10.game.map;
+
+public class WorldLoader {
+
+    public WorldLoader()
+    {
+
+    }
+
+    public World load(String filename)
+    {
+        return null;
+    }
+
+    public World load(byte[] bytes)
+    {
+        return null;
+    }
+
+    public void generate(World world,String path)
+    {
+
+    }
 }
