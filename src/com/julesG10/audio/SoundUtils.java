bf214791032c2e13cb7cdac359aa73a33872d078
JulesG10 <jules10avrg@gmail.com>
2021-08-17 17:30:26 +0200

    sound class

---
+++ src/com/julesG10/audio/SoundUtils.java
@@ -0,0 +1,32 @@
+package com.julesG10.audio;
+
+import com.julesG10.Main;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class SoundPlayer {
+
+    private static int soundId = 0;
+
+    public static Sound load(String path)
+    {
+        try {
+            Clip clip = AudioSystem.getClip();
+            AudioInputStream inputStream = AudioSystem.getAudioInputStream(
+                    Main.class.getResourceAsStream(path));
+            clip.open(inputStream);
+
+            Sound sound = new Sound(clip,soundId);
+            soundId++;
+
+            return sound;
+        } catch (Exception e)
+        {
+            return null;
+        }
+    }
+}
+
+
