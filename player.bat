3f2c839d8b7343f4d21b144ff97c7291b56e0bce
JulesG10 <jules10avrg@gmail.com>
2021-08-26 14:48:36 +0200

    player + server tests

---
+++ player.bat
@@ -0,0 +1,3 @@
+@echo off
+cd C:\Users\jules\IdeaProjects\OpenBattles\out\artifacts\OpenBattles_jar\
+C:\Users\jules\.jdks\openjdk-16.0.2\bin\java.exe -jar OpenBattles.jar -server=false -public=false -port=8080 -address=192.168.1.16 -window=true -fullscreen=false -size=500x500
---
+++ server.bat
@@ -0,0 +1,3 @@
+@echo off
+cd C:\Users\jules\IdeaProjects\OpenBattles\out\artifacts\OpenBattles_jar\
+C:\Users\jules\.jdks\openjdk-16.0.2\bin\java.exe -jar OpenBattles.jar -window=false -server=true -public=false -port=8080
