52940c6714a4e6779de756512eea771e100436b9
JulesG10 <jules10avrg@gmail.com>
2021-09-09 17:47:36 +0200

    shader update

--- src/com/julesG10/graphics/Shader.java
+++ src/com/julesG10/graphics/Shader.java
@@ -2,43 +2,75 @@ package com.julesG10.graphics;
 
 import org.lwjgl.BufferUtils;
 
+import java.io.IOException;
 import java.nio.IntBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static org.lwjgl.opengl.GL20.*;
 
 public class Shader {
     private int id;
 
+
+    public static String load(String path)
+    {
+        if(path.endsWith(".glsl") || path.endsWith(".vert") || path.endsWith(".frag"))
+        {
+            try {
+                return  Files.readAllLines(Path.of(path)).toString();
+            } catch (IOException ignored) {}
+        }
+        return null;
+    }
+
     public Shader(String vertexSource,String fragmentSource)
     {
         int vertexShader = createShader(GL_VERTEX_SHADER, vertexSource);
         int fragmentShader = createShader(GL_FRAGMENT_SHADER, fragmentSource);
+        if(vertexShader == -1 || fragmentShader == -1)
+        {
+            return;
+        }
 
         id = glCreateProgram();
         glAttachShader(id, vertexShader);
         glAttachShader(id, fragmentShader);
 
         glLinkProgram(id);
-
-        IntBuffer buffer = BufferUtils.createIntBuffer(0);
-        glGetProgramiv(id, GL_LINK_STATUS, buffer);
-
-        glDetachShader(id, vertexShader);
+        int comp = glGetProgrami(id, GL_LINK_STATUS);
+        if(comp == GL_FALSE)
+        {
+            return;
+        }
+        /*glDetachShader(id, vertexShader);
         glDetachShader(id, fragmentShader);
+
         glDeleteShader(vertexShader);
         glDeleteShader(fragmentShader);
+        */
     }
 
-    private int createShader(int type,String src)
+
+    private int createShader(int type,String source)
     {
-        int id = glCreateShader(type);
+        int shader = glCreateShader(type);
 
-        glShaderSource(id,  src);
-        glCompileShader(id);
+        if (shader == 0)
+        {
+            return -1;
+        }
 
-        IntBuffer buffer = BufferUtils.createIntBuffer(0);
-        glGetShaderiv(id, GL_COMPILE_STATUS,buffer);
+        glShaderSource(shader, source);
+        glCompileShader(shader);
 
-        return id;
+        int comp = glGetShaderi(shader, GL_COMPILE_STATUS);
+
+        if (comp == GL_FALSE)
+        {
+            return -1;
+        }
+
+        return shader;
     }
 }
